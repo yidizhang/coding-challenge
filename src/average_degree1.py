9059210c3a8238e2aaccfd03247ed5d3f9133ff4
yidizhang <zcj1022@hotmail.com>
2016-04-08 19:26:30 -0400

    Added files via upload
    first try

---
+++ src/average_degree1.py
@@ -0,0 +1,119 @@
+import sys
+import json
+import time
+import itertools
+import datetime
+
+def getHashTags(hashtags):
+    tags = []
+    for tag in hashtags:
+        tag = (tag["text"])
+        if len(tag) > 0:
+            tags.append(tag)
+    return list(set(tags))
+
+def lessThanAMinute(time1,time2):
+    ts1 =  datetime.datetime.strptime(time1,'%a %b %d %H:%M:%S +0000 %Y')
+    ts2 =  datetime.datetime.strptime(time2,'%a %b %d %H:%M:%S +0000 %Y')
+    tDelta = ts2 - ts1
+    return tDelta.total_seconds() <= 60
+
+def lessThan(time1, time2):
+    ts1 =  datetime.datetime.strptime(time1,'%a %b %d %H:%M:%S +0000 %Y')
+    ts2 =  datetime.datetime.strptime(time2,'%a %b %d %H:%M:%S +0000 %Y')
+    tDelta = ts2 - ts1
+    return tDelta.total_seconds() >= 0
+
+def findMaxTime(tweets):
+    for i in range(len(tweets)):
+        if (lessThan(tweets[0]["created_at"],tweets[i]["created_at"])):
+            max = tweets[i]["created_at"]
+    return max
+
+def getHashTagLinks(tags):
+    return itertools.permutations(tags,2)
+
+def printUsage():
+    print "Usage: python average_degree.py <input_file> <output_file>"
+
+def generateGraph(inputTweets):
+    tweets = [];
+    for t in inputTweets:
+        t = json.loads(t)
+        if not("created_at" in t or "text" in t):
+            continue
+        tweet = {}
+        tweet["created_at"] =  t["created_at"]
+        tweet["hashtags"] = getHashTags(t["entities"]["hashtags"])
+        tweets.append(tweet)
+        time_current_tweets = findMaxTime(tweets)
+        avgDegree = []
+        graph = {}
+
+        i = 0  # Current Tweet
+        for i in range(len(tweets)):
+            while(not lessThanAMinute(tweets[i]["created_at"],time_current_tweets)):
+                #Remove links
+                if len(tweets[i]["hashtags"]) > 1:
+                    removeEdges = getHashTagLinks(tweets[i]["hashtags"])
+                    for edge in removeEdges:
+                        graph[edge[0]].remove(edge[1])
+
+                i+=1
+
+
+            #Add Links
+            if len(tweets[i]["hashtags"]) > 1:
+                addEdges = getHashTagLinks(tweets[i]["hashtags"])
+                for edge in addEdges:
+                    if edge[0] in graph:
+                        graph[edge[0]].append(edge[1])
+                    else:
+                        graph[edge[0]] = [edge[1]]
+
+                #Count Degree
+            degree = 0.0
+            if len(graph) ==0:
+                avgDegree.append(format(0.00,'.2f'))
+            else:
+                for node in graph:
+                    degree += len(set((graph[node])))
+                avgDegree.append(format(round(degree/len(graph),2),'.2f'))
+
+    return avgDegree
+
+
+def main(argv):
+    if len(argv) < 2 :
+        printUsage()
+        sys.exit(2)
+    inputTweetsFile = argv[0]
+    outputTweetsFile = argv[1]
+    inputTweets = []
+
+    #Read Input File and save all input tweets in memory
+    with open(inputTweetsFile,"r") as fIn :
+        inputTweets = fIn.readlines()
+
+    #Extract HashTag List and Timestamp for each tweet
+    #inputTweets = extractHashTagsAndTimestamps(inputTweets)
+
+    #GenerateGraph for every tweet
+    output = generateGraph(inputTweets)
+
+    #Clean the Output File if it already exists
+    open(outputTweetsFile, 'w').close()
+
+    #Save Avg Degree for each incoming tweet
+    with open(outputTweetsFile,"w") as fOut:
+        for avgDegree  in output:
+            fOut.write(str(avgDegree) + "\n")
+
+
+
+
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
+
