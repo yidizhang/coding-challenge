42332e8bb1efe21a12a1a0d96fb867eb0bd44ef0
yidizhang <zcj1022@hotmail.com>
2016-04-09 00:01:34 -0400

    final version

--- src/average_degree1.py
+++ src/average_degree1.py
@@ -1,9 +1,9 @@
 import sys
 import json
-import time
 import itertools
 import datetime
 
+# the function to extract text field from hashtags
 def getHashTags(hashtags):
     tags = []
     for tag in hashtags:
@@ -12,33 +12,46 @@ def getHashTags(hashtags):
             tags.append(tag)
     return list(set(tags))
 
+
+# the function to check whether two tweets are within 60 secs
 def lessThanAMinute(time1,time2):
     ts1 =  datetime.datetime.strptime(time1,'%a %b %d %H:%M:%S +0000 %Y')
     ts2 =  datetime.datetime.strptime(time2,'%a %b %d %H:%M:%S +0000 %Y')
     tDelta = ts2 - ts1
     return tDelta.total_seconds() <= 60
 
+
+# the function to check whether time1 is less than time2
 def lessThan(time1, time2):
     ts1 =  datetime.datetime.strptime(time1,'%a %b %d %H:%M:%S +0000 %Y')
     ts2 =  datetime.datetime.strptime(time2,'%a %b %d %H:%M:%S +0000 %Y')
     tDelta = ts2 - ts1
     return tDelta.total_seconds() >= 0
 
+# the function to find maximaum timestamp of a tweet among the tweets which are being processed
 def findMaxTime(tweets):
     for i in range(len(tweets)):
         if (lessThan(tweets[0]["created_at"],tweets[i]["created_at"])):
             max = tweets[i]["created_at"]
     return max
 
+# the function to do 2 element permutation of a list
 def getHashTagLinks(tags):
     return itertools.permutations(tags,2)
 
+# the function to format how the code is being run
 def printUsage():
-    print "Usage: python average_degree.py <input_file> <output_file>"
+    print ("Usage: python average_degree.py <input_file> <output_file>")
 
+# the function to read one tweet at a time, add the tweets fall within 60 second window of the current maximum timestamp
+# tweet, remove the tweets which fall outside 60 secs window of the current maximum timestamp tweet, and compute the
+# average degree of vertices and keep updated this number.
 def generateGraph(inputTweets):
     tweets = [];
-    for t in inputTweets:
+    avgDegree = []
+    for tweetNumber, t in enumerate(inputTweets):
+        if tweetNumber % 20 == 0:
+            print('Processing tweet ' + str(tweetNumber))
         t = json.loads(t)
         if not("created_at" in t or "text" in t):
             continue
@@ -47,38 +60,33 @@ def generateGraph(inputTweets):
         tweet["hashtags"] = getHashTags(t["entities"]["hashtags"])
         tweets.append(tweet)
         time_current_tweets = findMaxTime(tweets)
-        avgDegree = []
         graph = {}
 
         i = 0  # Current Tweet
+        remaining_tweets = []
         for i in range(len(tweets)):
-            while(not lessThanAMinute(tweets[i]["created_at"],time_current_tweets)):
-                #Remove links
+            # Add Links
+            if(lessThanAMinute(tweets[i]["created_at"],time_current_tweets)):
+                remaining_tweets.append(tweet)
                 if len(tweets[i]["hashtags"]) > 1:
-                    removeEdges = getHashTagLinks(tweets[i]["hashtags"])
-                    for edge in removeEdges:
-                        graph[edge[0]].remove(edge[1])
+                    addEdges = getHashTagLinks(tweets[i]["hashtags"])
+                    for edge in addEdges:
+                        if edge[0] in graph:
+                            graph[edge[0]].append(edge[1])
+                        else:
+                            graph[edge[0]] = [edge[1]]
 
-                i+=1
 
+        #Count Degree
+        degree = 0.0
+        if len(graph) ==0:
+            avgDegree.append(format(0.00,'.2f'))
+        else:
+            for node in graph:
+                degree += len(set((graph[node])))
+            avgDegree.append(format(round(degree/len(graph),2),'.2f'))
 
-            #Add Links
-            if len(tweets[i]["hashtags"]) > 1:
-                addEdges = getHashTagLinks(tweets[i]["hashtags"])
-                for edge in addEdges:
-                    if edge[0] in graph:
-                        graph[edge[0]].append(edge[1])
-                    else:
-                        graph[edge[0]] = [edge[1]]
-
-                #Count Degree
-            degree = 0.0
-            if len(graph) ==0:
-                avgDegree.append(format(0.00,'.2f'))
-            else:
-                for node in graph:
-                    degree += len(set((graph[node])))
-                avgDegree.append(format(round(degree/len(graph),2),'.2f'))
+        tweets = remaining_tweets
 
     return avgDegree
 
@@ -95,9 +103,6 @@ def main(argv):
     with open(inputTweetsFile,"r") as fIn :
         inputTweets = fIn.readlines()
 
-    #Extract HashTag List and Timestamp for each tweet
-    #inputTweets = extractHashTagsAndTimestamps(inputTweets)
-
     #GenerateGraph for every tweet
     output = generateGraph(inputTweets)
 
@@ -108,10 +113,6 @@ def main(argv):
     with open(outputTweetsFile,"w") as fOut:
         for avgDegree  in output:
             fOut.write(str(avgDegree) + "\n")
-
-
-
-
 
 
 if __name__=="__main__":
